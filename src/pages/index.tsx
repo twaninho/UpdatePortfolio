4e19dee251052091d97bb5777ceadbb7112f8764
Twan Hopmans <twanjhopmans@hotmail.com>
2023-07-31 11:12:52 +0200

    making it more responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,14 +23,14 @@ export default function Home() {
       <Navbar />
       
       <section className="grid grid-cols-1 lg:grid-cols-[2fr_1fr] gap-4 mb-4">
-          <div className="sm:h-[30rem] h-[25rem] rounded-3xl p-10 flex flex-col gap-16 bg-[url('/gradient-bg.jpg')] bg-cover">
+          <div className="sm:h-[30rem] h-[25rem] rounded-3xl p-10 flex flex-col gap-12 bg-[url('/gradient-bg.jpg')] bg-cover">
             <h1 className="sm:text-4xl text-2xl font-semibold">Hello, I&apos;m a Web Developer with 6 months of experience.</h1>
             <p className="flex-1">I care a lot about using design for positive impact and enjoy
               creating user-centric, delightful, and human experiences.
             </p>
             <div className="flex flex-col lg:flex-row items-center gap-4 justify-self-end">
               <a href="#contact">
-              <button className="bg-black text-white font-medium py-3 px-12 rounded-full w-60 lg:w-auto hover:bg-button transition-all ease-in-out duration-300">Contact me</button>
+              <button className="bg-black text-white font-medium sm:py-3 sm:px-11 py-2 px-8 rounded-full lg:w-auto hover:bg-button transition-all ease-in-out duration-300">Contact me</button>
               </a>
               <div className="lg:flex items-center gap-4 hidden">
                 <SocialButton bgColor="github" socialLink="https://github.com/twaninho">
